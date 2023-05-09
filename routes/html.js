b18712a130bbaa6fffa1f70d7d61afd56bce2bce
Annalee Beltran <annalee.beltranusaf@gmail.com>
2023-05-08 22:58:37 -0500

    fixed some routes

--- routes/html.js
+++ routes/html.js
@@ -2,13 +2,13 @@ const path = require("path");
 const router = require("express").Router();
 
 // GET notes request
-router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // GET * request
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware, Data Parsing
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
 // Routes
