8a29680f442fc402734b7b371acd6ade5fa9a2ae
Annalee Beltran <annalee.beltranusaf@gmail.com>
2023-05-08 23:09:25 -0500

    fixed order

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const html = require("./routes/html");
 const api = require("./routes/api");
 
 // Express
-const app = express();
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Middleware, Data Parsing
-app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+app.use(express.static("public"));
 
 // Routes
 app.use("/api", api);
@@ -18,7 +21,7 @@ app.use("/", html);
 
 // Listener PORT
 app.listen(PORT, () => {
-  console.log(`App listening on http://localhost:${PORT}`);
+  console.log(`App listening on http://127.0.0.1:${PORT}`);
 });
 
 module.exports = app;
