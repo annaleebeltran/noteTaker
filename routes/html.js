f53b4a17e6b9875f834868665d0f4d57f228cebb
Annalee Beltran <annalee.beltranusaf@gmail.com>
2023-05-11 11:18:55 -0500

    fixed pathing issue

--- routes/html.js
+++ routes/html.js
@@ -3,12 +3,12 @@ const router = require("express").Router();
 
 // GET notes request
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../assets/public/index.html"));
 });
 
 // GET * request
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../assets/public/notes.html"));
 });
 
 module.exports = router;
